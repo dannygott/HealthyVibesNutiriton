5ceccca1c8beb6e873925166e34343412d326c1e
dgott <dgott@dbaza.com>
2023-07-23 14:42:37 -0400

    fix

--- edits.js
+++ edits.js
@@ -23,6 +23,7 @@
             }
                 if (paragraphs[i].textContent.includes("6040 Library Road, Bethel Park PA 15102")) {
                     // Replace the content with "an.cappello@healthyvibesnutritionist.com"
+                    console.log("Placed Location Link")
                     paragraphs[i].onclick = "location='https://www.google.com/maps?q=6040+Library+Road,+Bethel+Park+PA+15102'"
                 }
         }
