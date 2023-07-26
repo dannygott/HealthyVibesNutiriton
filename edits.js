3e83efe8d5cab01a7d8f301195ee52285b3cdd2b
dgott <dgott@dbaza.com>
2023-07-26 10:13:07 -0400

    fix contact

--- edits.js
+++ edits.js
@@ -31,7 +31,7 @@
                 var h1 = document.createElement("h1");
                 h1.textContent = "Order Now";
                 h1.style.color = "lightgreen";
-                h1.style.fontSize = "40px";
+                h1.style.fontSize = "40px !important";
                 h1.onclick = function () { window.location.href = 'https://wellevate.me/dan-capello'; };
                 // Append the h1 element to the parent div
                 var parentDiv = contact[i].parentNode;
